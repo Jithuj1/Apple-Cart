fbfbea5ff786b5d689b58546b92ed098f2e891e0
Jithu Jacob <jithujacob73@gmail.com>
2023-06-27 17:13:37 +0530

    debug true

--- store/settings.py
+++ store/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9lu(@1kwgshyew)7uw3c%y1r0!=2a1r_844r1kxqn4hs2dailr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
